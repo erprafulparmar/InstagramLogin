44ae07896b2d76061d1e2b25fb67090ec89bd02e
Er Praful Parmar <prafulparmar922@gmail.com>
2018-05-27 20:59:11 +0530

    Update StringUtils.java

--- app/src/main/java/com/praful/instagram/login/utils/StringUtils.java
+++ app/src/main/java/com/praful/instagram/login/utils/StringUtils.java
@@ -47,3 +47,115 @@ public class StringUtils {
 		return str;
 	}
 }
+
+
+
+
+
+
+ ========================================================================================================================================
+	
+	private static Context ctx;
+    private static FileInputStream stream;
+    private static CipherInputStream cis;
+    private static AssetFileDescriptor fileDescriptor;
+    private static String[] images = new String[0];
+    private static ArrayList<String> listImages;
+
+
+public static ArrayList<Bitmap> decryptDataFrame(Context applicationContext) throws IOException, NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException
+    {
+        ArrayList<Bitmap> arrBitmapList=new ArrayList<>();
+        try {
+            ctx = applicationContext;
+            AssetManager mngr = ctx.getAssets();
+            images = mngr.list("standImage");
+            listImages = new ArrayList<>(Arrays.asList(images));
+
+            for (int i = 0; i < images.length; i++)
+            {
+                fileDescriptor = mngr.openFd("standImage/" + listImages.get(i));
+                stream = fileDescriptor.createInputStream();
+                SecretKeySpec sks = new SecretKeySpec(DCStaticMethod.generateAPIKEY().getBytes(), "AES");
+                Cipher cipher = Cipher.getInstance("AES");
+                cipher.init(Cipher.DECRYPT_MODE, sks);
+                cis = new CipherInputStream(stream, cipher);
+                Bitmap mBitmap = BitmapFactory.decodeStream(cis, null, null);
+                arrBitmapList.add(mBitmap);
+                cis.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return arrBitmapList;
+    }
+	
+========================================================================================================================================	
+	
+	
+	
+	
+	
+	
+	
+	
+========================================================================================================================================
+	public static Context ctx;
+    public static FileInputStream stream;
+    public static CipherOutputStream cos;
+    public static AssetFileDescriptor fileDescriptor;
+    public static String[] arrimages = new String[0];
+    public static ArrayList<String> listImages;
+    public static String folder_main = "StandEncryptMedia";
+    public static int countI = 0;
+    public static int Size = 0;
+    Context context;
+    OnAsyncEncMediaResponse caller;
+    ProgressDialog pDialog = null;
+    String TAG = "AsyncEncMedia";
+    //public static String strKey = "HvX4JuGQciR9lv7q";
+    public static String strKey = "0905AIzaSyBHupT0";
+	
+	public static void encrypt(Context applicationContext) throws IOException, NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException {
+
+        ctx = applicationContext;
+        AssetManager mngr = ctx.getAssets();
+        arrimages = mngr.list("standImage");
+        listImages = new ArrayList<String>(Arrays.asList(arrimages));
+        for (countI = 0; countI < arrimages.length; countI++) {
+
+            fileDescriptor = mngr.openFd("standImage/" + listImages.get(countI));
+
+            File extStore = new File(Environment.getExternalStorageDirectory(), folder_main);
+            if (!extStore.exists()) {
+                extStore.mkdirs();
+            }
+            FileOutputStream fos = new FileOutputStream(extStore + "/" + arrimages[countI]);
+            stream = fileDescriptor.createInputStream();
+            SecretKeySpec sks = new SecretKeySpec(strKey.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, sks);
+            cos = new CipherOutputStream(fos, cipher);
+            int b;
+            byte[] d = new byte[8];
+            while ((b = stream.read(d)) != -1) {
+                cos.write(d, 0, b);
+            }
+            cos.flush();
+            cos.close();
+            stream.close();
+            Log.e("encrypted ", "--->" + "done");
+        }
+    }
+
+========================================================================================================================================
+
+
+
+
+
+
+
+
